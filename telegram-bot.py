eedd6d778304ad1d7de628c17f12a297b8455cfa
echochio <echochio-tw@users.noreply.github.com>
2019-03-14 15:06:51 +0800

    Update telegram-bot.py

--- telegram-bot.py
+++ telegram-bot.py
@@ -2,11 +2,16 @@
 # Telegram Remote-Shell
 
 from pprint import pprint
-import telepot,time,os
+import qrcode,telepot,time,os
 
 # Telegram senders id
-authorized_senders = "-3250000000"
-
+authorized_senders = "-3296xxxx"
+qr = qrcode.QRCode(
+    version = 1,
+    error_correction = qrcode.constants.ERROR_CORRECT_H,
+    box_size = 10,
+    border = 4,
+)
 
 def handle(msg):
     chat_id = msg['chat']['id']
@@ -19,12 +24,24 @@ def handle(msg):
       args=text.split()
       print(args[0]+" "+args[1]+"\n")
       command = args[0]
-      if command == '/refresh':
+      if command == '/flash':
             host = str(args[1])
             output=os.popen("/home/si/refresh "+host).read()
             bot.sendMessage(chat_id, output)
-
-bot = telepot.Bot('688781292:AAFfaS1Q6CFz6TAtlQ1xxxxxxxxxxx')
+      if command == '/zsy':
+            host = str(args[1])
+            output=os.popen("/root/zsy "+host).read()
+            bot.sendMessage(chat_id, output)
+      if command == '/qr':
+            host = str(args[1])
+            qr.add_data(host)
+            qr.make(fit=True)
+            img = qr.make_image()
+            img.save("/tmp/photo.png")
+            photo = open('/tmp/photo.png', 'rb')
+            bot.sendPhoto(chat_id, photo)
+bot = telepot.Bot('705891952:AAFA7QQR9b8znxxxxxxxxxxxxxxxxxxxxxxxxx')
 bot.message_loop(handle)
 while 1:
     time.sleep(10)
+

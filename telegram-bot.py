d354760e1255b2d576814433c48b4cadacd014aa
echochio <echochio-tw@users.noreply.github.com>
2019-03-23 22:30:00 +0800

    Update telegram-bot.py

--- telegram-bot.py
+++ telegram-bot.py
@@ -1,47 +1,40 @@
 #!/usr/bin/python
 # Telegram Remote-Shell
-
-from pprint import pprint
-import qrcode,telepot,time,os
+from datetime import datetime
+now = datetime.now()
+import telepot,time,os
 
 # Telegram senders id
-authorized_senders = "-3296xxxx"
-qr = qrcode.QRCode(
-    version = 1,
-    error_correction = qrcode.constants.ERROR_CORRECT_H,
-    box_size = 10,
-    border = 4,
-)
+authorized_senders = "-289xxxxxxx"
 
-def handle(msg):
-    chat_id = msg['chat']['id']
-    text = msg['text']
-    sender = msg['from']['id']
-    f = open('trsh.log', 'a')
-    f.write("Chat-id - "+str(chat_id)+" Text - "+str(text)+" Sender - "+str(sender)+"\n")
-    f.close()
-    if (authorized_senders == str(chat_id)):
-      args=text.split()
-      print(args[0]+" "+args[1]+"\n")
-      command = args[0]
-      if command == '/flash':
-            host = str(args[1])
-            output=os.popen("/home/si/refresh "+host).read()
-            bot.sendMessage(chat_id, output)
-      if command == '/zsy':
-            host = str(args[1])
-            output=os.popen("/root/zsy "+host).read()
-            bot.sendMessage(chat_id, output)
-      if command == '/qr':
-            host = str(args[1])
-            qr.add_data(host)
-            qr.make(fit=True)
-            img = qr.make_image()
-            img.save("/tmp/photo.png")
-            photo = open('/tmp/photo.png', 'rb')
-            bot.sendPhoto(chat_id, photo)
-bot = telepot.Bot('705891952:AAFA7QQR9b8znxxxxxxxxxxxxxxxxxxxxxxxxx')
-bot.message_loop(handle)
-while 1:
+try:
+    def handle(msg):
+        content_type, chat_type, chat_id = telepot.glance(msg)
+        sender = msg['from']['id']
+        f = open('trsh.log', 'a')
+        date_time = now.strftime("%m/%d/%Y, %H:%M:%S")
+        f.write("Chat-id - "+str(chat_id)+", content_type - "+str(content_type)+", Sender - "+str(sender)+", "+date_time+"\n")
+        f.close()
+        if (authorized_senders == str(chat_id)) and (content_type == 'text'):
+            text = msg['text']
+            args=text.split()
+            command = args[0]
+            if command == '/refresh':
+                host = str(args[1])
+                output=os.popen("/home/si/refresh "+host).read()
+                bot.sendMessage(chat_id, output)
+            else:
+                bot.sendMessage(chat_id,"Error input wait for 10 Seconds")
+                time.sleep(10)
+                os.popen("kill -9 `ps -ef |grep telegram-bot.py |grep -v grep |awk '{print $2}'`;python /home/si/telegram-bot.py &").read()
+        else:
+            time.sleep(10)
+            bot.sendMessage(chat_id,"Error input wait for 10 Seconds")
+            os.popen("kill -9 `ps -ef |grep telegram-bot.py |grep -v grep |awk '{print $2}'`;python /home/si/telegram-bot.py &").read()
+    bot = telepot.Bot('57349xxxxx:AAEDZucDqHxxxxxxxxxxxxxxxx')
+    bot.message_loop(handle)
+    while 1:
+        time.sleep(10)
+except:
     time.sleep(10)
-
+    os.popen("kill -9 `ps -ef |grep telegram-bot.py |grep -v grep |awk '{print $2}'`;python /home/si/telegram-bot.py &").read()
